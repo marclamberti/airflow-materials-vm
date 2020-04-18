4e91f2d7b0b78efda2507130cc763832c1e44610
Marc LAMBERTI <marclamberti.ml@gmail.com>
2020-04-18 15:26:07 +0200

    update virtualenv

--- vagrant/scripts/setup-virtualenv.sh
+++ vagrant/scripts/setup-virtualenv.sh
@@ -5,10 +5,16 @@
 source "/vagrant/scripts/common.sh"
 
 function installVenv {
-    apt-get install python3-setuptools
-    easy_install3 pip
-    pip install virtualenv
-    virtualenv .sandbox
+    apt-get -y install build-essential checkinstall
+    apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+    cd /usr/src
+    wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
+    tar xzf Python-3.5.6.tgz
+    cd Python-3.5.6
+    ./configure --enable-optimizations
+    make altinstall
+    cd /home/vagrant
+    python3.5 -m venv .sandbox
     chmod +x .sandbox/bin/activate
 }
 
