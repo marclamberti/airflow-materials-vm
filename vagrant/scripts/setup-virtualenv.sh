f41ad6c2415d4832a7fab288a060f21b8c00ca44
Marc LAMBERTI <marclamberti.ml@gmail.com>
2020-04-18 16:05:47 +0200

    chown

--- vagrant/scripts/setup-virtualenv.sh
+++ vagrant/scripts/setup-virtualenv.sh
@@ -7,6 +7,7 @@ source "/vagrant/scripts/common.sh"
 function installVenv {
     apt-get -y install build-essential checkinstall
     apt-get -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+    apt-get install libmysqlclient-dev
     cd /usr/src
     wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tgz
     tar xzf Python-3.5.6.tgz
@@ -14,8 +15,10 @@ function installVenv {
     ./configure --enable-optimizations
     make altinstall
     cd /home/vagrant
+    pip install --upgrade pip
     python3.5 -m venv .sandbox
     chmod +x .sandbox/bin/activate
+    chown -R vagrant: . 
 }
 
 echo "setup python virtual environment"
